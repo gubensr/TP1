140683f7a7c4ab3ec281cfab14697b13116bf40e
morghadilias <morghadilias7@gmail.com>
2024-11-13 10:40:48 -0500

    commit tom 7.1

---
+++ BankServer/src/main/java/com/atoudeft/banque/Operation.java
@@ -0,0 +1,26 @@
+package com.atoudeft.banque;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public abstract class Operation implements Serializable {
+    private TypeOperation type;
+    private Date date;
+
+    public Operation(TypeOperation type) {
+        this.type = type;
+        this.date = new Date(System.currentTimeMillis());
+    }
+
+    public TypeOperation getType() {
+        return type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    // Méthodes abstraites que les sous-classes devront implémenter
+    public abstract void execute();
+}
+
